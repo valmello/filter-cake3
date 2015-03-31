f9080f854dbaebcea59ed977490ef38fd5d8302d
André <cavallari@live.com>
2015-03-30 23:06:26 -0300

    New filter updates paginate variable

--- src/Controller/Component/FilterComponent.php
+++ src/Controller/Component/FilterComponent.php
@@ -1,49 +1,48 @@
-<?PHP
+<?php
 namespace Filter\Controller\Component;
 use Cake\Controller\Component;
+use Cake\Controller\ComponentRegistry;
 
-class FilterComponent extends Component{
+class FilterComponent extends Component
+{
 
-    public function initialize(array $config) {
+    public function initialize(array $config)
+    {
         parent::initialize($config);
-    }
-
-    public function __get($method){
-        $method = "_get".ucfirst($method);
-        if(method_exists($this,$method)) return $this->$method();
-        return null;
-    }
-
-    protected function _getConditions(){
         $controller = $this->_registry->getController();
-        $queries = $controller->request->query;
-        $conditions = [];
         if(empty($controller->filters)) return null;
-        if(empty($queries)) return null;
+        $query = &$controller->request->query;
+        $data = &$controller->request->data;
+        $paginate = &$controller->paginate;
         foreach($controller->filters as $key=>$filter){
-            if(is_numeric($key) || empty($filter['field']) || empty($queries[$key])) continue;
+            if(is_numeric($key) || empty($filter) || empty($query[$key])) continue;
             if(empty($filter['type'])) $filter['type'] = 'text';
-            $method = "__get".ucfirst($filter['type']);
-            $filter['value'] = $queries[$key];
-            $conditions += $this->$method($filter);
-            $controller->request->data[$key] = $queries[$key];
+            if(empty($paginate[$filter['model']]['conditions'])) $paginate[$filter['model']]['conditions'] = [];
+            $condition = &$paginate[$filter['model']]['conditions'];
+            $method = '_get' . ucfirst($filter['type']);
+            $filter['value'] = $query[$key];
+            $condition[] = $this->$method($filter);
+            $data[$key] = $query[$key];
         }
-        return $conditions;
     }
 
-    protected function __getText($options){
-        $value = preg_replace('/"([\w\d\s-_?!#$]+)"|([\w\d]+)/u','%$1$2%',$options['value']);
-        $field = $options['field'];
-        return ["{$field} LIKE"=>$value];
-    }
-
-    protected function __getRange($options){
+    protected function _getRange($options)
+    {
         $options +=['separator'=>';'];
         $values = explode($options['separator'],$options['value']);
-        return ["{$options['field']} BETWEEN {$values[0]} AND {$values[1]}"];
+        return "{$options['model']}.{$options['field']} BETWEEN {$values[0]} AND {$values[1]}";
     }
 
-    protected function __getMultiple($options){
-        return ["{$options['field']} IN"=>$options['value']];
+    protected function _getText($options)
+    {
+        $value = preg_replace('/"([\w\d\s-_?!#$]+)"|([\w\d]+)/u','%$1$2%',$options['value']);
+        return "{$options['model']}.{$options['field']} LIKE {$value}";
     }
+
+    protected function _getMultiple($options)
+    {
+        return "{$options['model']}.{$options['field']} IN {$options['value']}";
+    }
+
+
 }
